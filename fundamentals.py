9906f9d5ac1daa03aa040604f2d394ee2d0338cf
Jingu Kang <jinguk0816@gmail.com>
2023-01-20 02:45:39 -0800

    write print, different variables and functions with parameters

---
+++ fundamentals.py
@@ -0,0 +1,30 @@
+# Print hello world
+print("hello world")
+
+# Variables
+# Javascript usually uses camel case(myAge) but python mainly uses snake case (my_age)
+a = 2 # an integer gets stored into a memory
+b = 3
+c = a + b
+c = 1 # overwrites the top code
+print(c)
+
+# Booleans and strings
+my_name = "Jingu"
+print(my_name)
+
+my_bool = True #first letter has to be a capital letter (True | False)
+print(my_bool)
+
+
+# Functions
+def say_hello():
+    print("hello how are you?")
+
+say_hello()
+
+# Parameters
+def say_bye(message):
+    print("Someone said:", message)
+
+say_bye("Hello Jingu!")
