94208987a5643dee2c4988191508f2df23613f91
Jingu Kang <jinguk0816@gmail.com>
2023-01-21 21:00:29 -0800

    learn for loop

--- fundamentals.py
+++ fundamentals.py
@@ -137,3 +137,16 @@ print(player)
 player["fav_food"].append("Ramen")
 print(player)
 
+# For loop
+
+websites = (
+    "google.com",
+    "airbnb.com",
+    "https://twitter.com",
+    "facebook.com",
+    "https://apple.com"
+)
+
+for website in websites:
+    if not website.startswith("https://"):
+        website = f"https://{website}"
