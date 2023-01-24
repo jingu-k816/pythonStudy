f7ecf4ba90f5c5ff637e8ee4d59fc2fbb3e25e2e
Jingu Kang <jinguk0816@gmail.com>
2023-01-23 18:51:21 -0800

    create a web scrape for indeed jobs with the keyword and make it a function

--- web_scrape/indeed.py
+++ web_scrape/indeed.py
@@ -3,24 +3,40 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
+def extract_indeed_jobs(keyword):
+    options = Options()
 
-browser = webdriver.Chrome(options=options)
+    browser = webdriver.Chrome(options=options)
 
-base_url = "https://ca.indeed.com/jobs?q="
-search_term = "junior+developer"
+    base_url = "https://ca.indeed.com/jobs?q="
 
-response = browser.get(f"{base_url}{search_term}")
-response = browser.page_source
+    response = browser.get(f"{base_url}{keyword}")
+    response = browser.page_source
 
-soup = BeautifulSoup(browser.page_source, "html.parser")
-job_list = soup.find("ul", class_="jobsearch-ResultsList")
+    results = []
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    job_list = soup.find("ul", class_="jobsearch-ResultsList")
 
-jobs = job_list.find_all("li", recursive=False)
-for job in jobs:
-    zone = job.find("div", class_="mosaic-zone")
+    jobs = job_list.find_all("li", recursive=False)
+    for job in jobs:
+        zone = job.find("div", class_="mosaic-zone")
 
-    if zone == None:
-        print("job li")
-    else:
-        print("mosaic li")
+        if zone == None:
+            anchor = job.select_one("h2 a")
+            print(anchor)
+            title = job.find("h2", class_="jobTitle")
+            link = anchor['href']
+            company = job.find("span", class_="companyName")
+            location = job.find("div", class_="companyLocation")
+
+            job_data = {
+                'link': f"https://ca.indeed.com{link}",
+                'company': company.string,
+                'location': location.string,
+                'position': title.string
+            }
+            results.append(job_data)
+
+    for result in results:
+        print(result)
+
