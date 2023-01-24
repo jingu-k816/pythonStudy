9f6fada52d67cbe8ae33511aab30a25eff22f9bd
Jingu Kang <jinguk0816@gmail.com>
2023-01-23 23:34:10 -0800

    learn basic flask and using templates

--- web_scrape/main.py
+++ web_scrape/main.py
@@ -1,12 +1,26 @@
-from wework_remote import extract_wwr_jobs
-from indeed import extract_indeed_jobs
-from file import save_to_file
+# from wework_remote import extract_wwr_jobs
+# from indeed import extract_indeed_jobs
+# from file import save_to_file
 
-keyword = input("What do you want to search for?")
+# keyword = input("What do you want to search for?")
 
-wwJobs = extract_wwr_jobs(keyword)
-indeedJobs = extract_indeed_jobs(keyword)
+# wwJobs = extract_wwr_jobs(keyword)
+# indeedJobs = extract_indeed_jobs(keyword)
 
-jobs = indeedJobs + wwJobs
+# jobs = indeedJobs + wwJobs
 
-save_to_file(keyword, jobs)
+# save_to_file(keyword, jobs)
+
+from flask import Flask, render_template
+
+app = Flask("JobScrapper", template_folder="web_scrape/templates")
+
+@app.route("/")
+def home():
+    return render_template("home.html", name="Jingu")
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
+
+app.run()
