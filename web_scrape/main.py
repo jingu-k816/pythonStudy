89cea7ddbf2d37c29798b8ae6d709d065c97e792
Jingu Kang <jinguk0816@gmail.com>
2023-01-24 18:37:43 -0800

    grab keyword from the URI and show the keyword in the search page

--- web_scrape/main.py
+++ web_scrape/main.py
@@ -1,17 +1,14 @@
-# from wework_remote import extract_wwr_jobs
-# from indeed import extract_indeed_jobs
-# from file import save_to_file
+from wework_remote import extract_wwr_jobs
+from indeed import extract_indeed_jobs
+from file import save_to_file
 
 # keyword = input("What do you want to search for?")
 
-# wwJobs = extract_wwr_jobs(keyword)
-# indeedJobs = extract_indeed_jobs(keyword)
 
-# jobs = indeedJobs + wwJobs
 
 # save_to_file(keyword, jobs)
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("JobScrapper", template_folder="web_scrape/templates")
 
@@ -21,6 +18,12 @@ def home():
 
 @app.route("/search")
 def search():
-    return render_template("search.html")
+    keyword = request.args.get("keyword")
+    wwJobs = extract_wwr_jobs(keyword)
+    indeedJobs = extract_indeed_jobs(keyword)
+
+    jobs = indeedJobs + wwJobs
+
+    return render_template("search.html", keyword=keyword)
 
 app.run()
